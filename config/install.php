db3ad602d1f4da12a6f226e4a82e62709fa67866
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 12:13:02 +0300

    Файлы конфигурации

---
+++ config/install.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'id'          => 'gm.be.config.page',
+    'moduleId'    => 'gm.be.config',
+    'name'        => 'Pages',
+    'description' => 'Setting the display of information on the page',
+    'namespace'   => 'Gm\Backend\Config\Page',
+    'path'        => '/gm/gm.be.config.page',
+    'route'       => 'page',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'permissions' => ['any', 'info'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM MS'],
+        ['app', 'code' => 'GM CMS'],
+        ['app', 'code' => 'GM CRM'],
+        ['module', 'id' => 'gm.be.config']
+    ]
+];
